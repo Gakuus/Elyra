000e7543f78fb6b2a0e8ce840f0415335fa67f5b
Gakuus <ts503488@gmail.com>
2026-07-06 23:23:56 -0300

    fix: CSP bloqueaba embed PDF (object-src 'none') + modal tamaño hoja
    - public/index.php: object-src 'self' y frame-src 'self' en CSP
      (estaba 'none', bloqueaba carga del PDF en el embed)
    - Modal cambiado de fullscreen a modal-xl con scroll,
      tamaño tipo hoja A4, se puede bajar haciendo scroll
    - embed con min-height: 80vh para ocupar buen espacio

--- views/publico/documento.php
+++ views/publico/documento.php
@@ -51,21 +51,19 @@
 </div>
 
 <div class="modal fade" id="docPublicoModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen modal-dialog-centered">
+    <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content bg-body">
-            <div class="modal-header border-bottom-0">
-                <div class="me-3 overflow-hidden">
-                    <h5 class="modal-title text-truncate" id="publicoPreviewTitle"></h5>
-                </div>
-                <div class="d-flex gap-2 flex-shrink-0">
+            <div class="modal-header">
+                <h5 class="modal-title text-truncate" id="publicoPreviewTitle"></h5>
+                <div class="d-flex gap-2 flex-shrink-0 ms-2">
                     <a id="publicoPreviewDownload" class="btn btn-sm btn-primary" title="Descargar PDF">
                         <i class="bi bi-download"></i>
                     </a>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
                 </div>
             </div>
-            <div class="modal-body p-0 d-flex">
-                <embed id="publicoPreviewEmbed" type="application/pdf" class="w-100" style="height: 90vh; border: none;">
+            <div class="modal-body p-0">
+                <embed id="publicoPreviewEmbed" type="application/pdf" class="w-100" style="min-height: 80vh; border: none;">
             </div>
         </div>
     </div>
